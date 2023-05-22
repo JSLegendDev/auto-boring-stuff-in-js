7a894afaed7f4eb7e47b306bf478417e531374cc
Nassim's Software <128189209+JSLegendDev@users.noreply.github.com>
2023-05-22 12:20:38 -0400

    added code examples for chapter 7

---
+++ chapter-7/caretAndDollarSign.js
@@ -0,0 +1,40 @@
+// The caret and dollar sign characters
+// ^ means a match must occur at the beginning
+const beginWithHello = /^Hello/
+console.log('Hello, World!'.match(beginWithHello))
+
+const endWithNumber = /\d$/
+console.log('Your number is 42'.match(endWithNumber))
+console.log('Your number is forty two.'.match(endWithNumber))
+
+const wholeStringIsNum = /^\d+$/
+console.log('1234567890'.match(wholeStringIsNum))
+console.log('12345xyz67890'.match(wholeStringIsNum))
+console.log('12 34567890'.match(wholeStringIsNum))
+
+// The wildcard character
+// . is called wildcard and will match every character
+// except a newline
+const atRegex = /.at/g
+console.log(
+    ...'The cat in the hat sat on the flat mat.'
+    .matchAll(atRegex)
+)
+
+const nameRegex = /First Name: (.*) Last Name: (.*)/
+const matchObject = 'First Name: Al Last Name: Sweigart'.match(nameRegex)
+console.log(matchObject)
+console.log(matchObject[1])
+console.log(matchObject[2])
+
+// .* is greedy by default. Use ? to make it non-greedy.
+const nonGreedyRegex = /<.*?>/
+console.log('<To serve man> for dinner.>'.match(nonGreedyRegex))
+
+const greedyRegex = /<.*>/
+console.log('<To serve man> for dinner.>')
+
+// Matching newlines with the dot character by using the s flag.
+const text = 'Serve the public trust.\nProtect the innocent.\nUphold the law.'
+console.log(text.match(/.*/))
+console.log(text.match(/.*/s))
--- chapter-7/characterClasses.js
+++ chapter-7/characterClasses.js
@@ -1,1 +1,37 @@
-const ingredientRegex = //
+/* 
+    Character classes
+    \d Any numeric digit from 0 to 9.
+    \D Any character that is not a numeric digit from 0 to 9.
+    \w Any letter, numeric digit, or the underscore character.
+    (Think of this as matching “word” characters.)
+    \W Any character that is not a letter, numeric digit, or the
+    underscore character.
+    \s Any space, tab, or newline character. (Think of this as
+    matching “space” characters.)
+    \S Any character that is not a space, tab, or newline.
+*/
+const ingredientRegex = /\d+\s\w+/g
+console.log(
+    [
+        ...'12 eggs, 7 carrots, 5 tomatoes, 4 cucumbers'
+        .matchAll(ingredientRegex)
+    ]
+)
+
+// Making your own character classes
+const vowelRegex = /[aeiouAEIOU]/g
+console.log(
+    [
+        ...'RoboCop eats baby food. BABY FOOD.'
+        .matchAll(vowelRegex)
+    ]
+)
+
+// negative character class
+const consonantRegex = /[^aeiouAEIOU]/g
+console.log(
+    [
+        ...'RoboCop eats baby food. BABY FOOD.'
+        .matchAll(consonantRegex)
+    ]
+)
